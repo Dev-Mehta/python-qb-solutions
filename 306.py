63c775a0fcf37268b4b67c9fde4a49f4981a6c82
Dev-Mehta <devbmehta04@gmail.com>
2023-12-26 11:19:12 +0530

    Added 306

--- 306.py
+++ 306.py
@@ -1,1 +1,18 @@
-""""""
+"""
+366 - Write a python program to check if a string is palindrome or not
+"""
+
+
+def isPalindrome(string: str) -> bool:
+    """
+    Returns whether the given string is palindrome or not
+    Parameters:
+        string (str): The string to check whether it is palindrome
+    Returns:
+        bool: True if string is palindrome else False
+    """
+    return string == string[::-1]
+
+s = input("Enter a string: ")
+output = "Given string is palindrome" if isPalindrome(s) else "Given string is not palindrome"
+print(output)
