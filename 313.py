13d3076c3e70d695098651323b62f4c17780f183
Dev-Mehta <devbmehta04@gmail.com>
2023-12-26 11:50:36 +0530

    Added 313

---
+++ 313.py
@@ -0,0 +1,12 @@
+"""
+313 - Write a python program to find all occurences of substring, ignore case.
+"""
+
+def countSubstring(string: str, substring: str, ignoreCase=True) -> int:
+    if ignoreCase:
+        string = string.lower()
+    return string.count(substring)
+
+s = input("Enter a string: ")
+sub = input("Enter a substring: ")
+print(f"{sub} occured {countSubstring(s, sub)} times in {s}")
